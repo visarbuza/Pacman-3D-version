ce7021b3a633ae620545735defb4cf93eaf12272
Visar Buza <visar@gjirafa.com>
2020-12-21 22:41:25 +0100

    Added sound, and setting it up for windows for the first time.

--- include/camera.h
+++ include/camera.h
@@ -12,7 +12,6 @@
 // input methods
 enum Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT, RUN, STOP_RUN };
 
-
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
@@ -23,121 +22,121 @@ const float ZOOM = 45.0f;
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for
 // use in OpenGL
 class Camera {
- public:
-  // Camera Attributes
-  glm::vec3 Position;
-  glm::vec3 Front;
-  glm::vec3 Up;
-  glm::vec3 Right;
-  glm::vec3 WorldUp;
-  float running = false;
+   public:
+    // Camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    float running = false;
 
-  // Euler Angles
-  float Yaw;
-  float Pitch;
+    // Euler Angles
+    float Yaw;
+    float Pitch;
 
-  // Camera options
-  float MovementSpeed;
-  float MouseSensitivity;
+    // Camera options
+    float MovementSpeed;
+    float MouseSensitivity;
 
-  // Constructor with vectors
-  Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW,
-         float pitch = PITCH)
-      : Front(glm::vec3(0.0f, 0.0f, 1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY) {
-    Position = position;
-    WorldUp = up;
-    Yaw = yaw;
-    Pitch = pitch;
-    updateCameraVectors();
-  }
-
-  // Constructor with scalar values
-  Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
-      : Front(glm::vec3(0.0f, 0.0f, 1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY) {
-    Position = glm::vec3(posX, posY, posZ);
-    WorldUp = glm::vec3(upX, upY, upZ);
-    Yaw = yaw;
-    Pitch = pitch;
-    updateCameraVectors();
-  }
-
-  // Returns the view matrix calculated using Euler Angles and the LookAt Matrix
-  // First Person
-  glm::mat4 GetFirstPersonView() { return glm::lookAt(Position, Position + Front, Up); }
-
-  // Third person view
-  glm::mat4 GetThirdPersonView() {
-    return glm::lookAt(glm::vec3(Position.x - (Front.x), 1.0, Position.z - Front.z), Position + Front, Up);
-  }
-
-  // Birds eye view
-  glm::mat4 Get2DView() {
-    return glm::lookAt(glm::vec3(Position.x, 15.0f, Position.z), Position, glm::vec3(0.0f, 0.0f, -1.0f));
-  }
-
-  // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined
-  // ENUM (to abstract it from windowing systems)
-  void ProcessKeyboard(Camera_Movement input, float deltaTime) {
-    float velocity = MovementSpeed * deltaTime;
-    if (input == FORWARD) Position += Front * velocity;
-    if (input == BACKWARD) Position -= Front * velocity;
-    if (input == LEFT) Position -= Right * velocity;
-    if (input == RIGHT) Position += Right * velocity;
-    if (input == RUN && !running) {
-      MovementSpeed *= 1.7;
-      running = true;
-    }
-    if (input == STOP_RUN && running) {
-      MovementSpeed /= 1.7;
-      running = false;
-    } 
-
-    if (!Config::flightEnabled) {
-      Position.y = 0.0f;      
-    }
-  }
-
-  // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
-  void ProcessMouseMovement(float xPos, float yPos, GLboolean constrainPitch = true) {
-    // Calculate offset based on last mouse position relative to current mouse position
-    float xoffset = (xPos - mouseLastX) * MouseSensitivity;
-    float yoffset = (mouseLastY - yPos) * MouseSensitivity;
-
-    // Update last position to current
-    mouseLastX = xPos;
-    mouseLastY = yPos;
-
-    Yaw = glm::mod(Yaw + xoffset, 360.0f);
-    ;
-    Pitch += yoffset;
-
-    // Make sure that when pitch is out of bounds, screen doesn't get flipped
-    if (constrainPitch) {
-      if (Pitch > 89.0f) Pitch = 89.0f;
-      if (Pitch < -89.0f) Pitch = -89.0f;
+    // Constructor with vectors
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           float yaw = YAW, float pitch = PITCH)
+        : Front(glm::vec3(0.0f, 0.0f, 1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY) {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
     }
 
-    // Update Front, Right and Up Vectors using the updated Euler angles
-    updateCameraVectors();
-  }
+    // Constructor with scalar values
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+        : Front(glm::vec3(0.0f, 0.0f, 1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY) {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
 
- private:
-  // Default mouse position values
-  GLfloat mouseLastX = 400, mouseLastY = 300;
+    // Returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    // First Person
+    glm::mat4 GetFirstPersonView() { return glm::lookAt(Position, Position + Front, Up); }
 
-  // Calculates the front vector from the Camera's (updated) Euler Angles
-  void updateCameraVectors() {
-    // Calculate the new Front vector
-    glm::vec3 front;
-    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-    front.y = sin(glm::radians(Pitch));
-    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-    Front = glm::normalize(front);
-    // Also re-calculate the Right and Up vector
-    Right =
-        glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the
-                                                     // more you look up or down which results in slower movement.
-    Up = glm::normalize(glm::cross(Right, Front));
-  }
+    // Third person view
+    glm::mat4 GetThirdPersonView() {
+        return glm::lookAt(glm::vec3(Position.x - (Front.x), 1.0, Position.z - Front.z), Position + Front, Up);
+    }
+
+    // Birds eye view
+    glm::mat4 Get2DView() {
+        return glm::lookAt(glm::vec3(Position.x, 15.0f, Position.z), Position, glm::vec3(0.0f, 0.0f, -1.0f));
+    }
+
+    // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera
+    // defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(Camera_Movement input, float deltaTime) {
+        float velocity = MovementSpeed * deltaTime;
+        if (input == FORWARD) Position += Front * velocity;
+        if (input == BACKWARD) Position -= Front * velocity;
+        if (input == LEFT) Position -= Right * velocity;
+        if (input == RIGHT) Position += Right * velocity;
+        if (input == RUN && !running) {
+            MovementSpeed *= 1.7;
+            running = true;
+        }
+        if (input == STOP_RUN && running) {
+            MovementSpeed /= 1.7;
+            running = false;
+        }
+
+        if (!Config::flightEnabled) {
+            Position.y = 0.0f;
+        }
+    }
+
+    // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void ProcessMouseMovement(float xPos, float yPos, GLboolean constrainPitch = true) {
+        // Calculate offset based on last mouse position relative to current mouse position
+        float xoffset = (xPos - mouseLastX) * MouseSensitivity;
+        float yoffset = (mouseLastY - yPos) * MouseSensitivity;
+
+        // Update last position to current
+        mouseLastX = xPos;
+        mouseLastY = yPos;
+
+        Yaw = glm::mod(Yaw + xoffset, 360.0f);
+        ;
+        Pitch += yoffset;
+
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch) {
+            if (Pitch > 89.0f) Pitch = 89.0f;
+            if (Pitch < -89.0f) Pitch = -89.0f;
+        }
+
+        // Update Front, Right and Up Vectors using the updated Euler angles
+        updateCameraVectors();
+    }
+
+   private:
+    // Default mouse position values
+    GLfloat mouseLastX = 400, mouseLastY = 300;
+
+    // Calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors() {
+        // Calculate the new Front vector
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        Front = glm::normalize(front);
+        // Also re-calculate the Right and Up vector
+        Right = glm::normalize(
+            glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the
+                                          // more you look up or down which results in slower movement.
+        Up = glm::normalize(glm::cross(Right, Front));
+    }
 };
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -1,143 +1,144 @@
 #include "game.h"
 
 void Game::init() {
-  state = GAME_MENU;
-  shader = Shader("../resources/shaders/vertex.vert", "../resources/shaders/fragment.frag");
-  text = TextRenderer(Config::SCR_WIDTH, Config::SCR_HEIGHT);
-  text.load("resources/fonts/ocraext.TTF", 24);
+    state = GAME_MENU;
+    shader = Shader("./resources/shaders/vertex.vert", "./resources/shaders/fragment.frag");
+    text = TextRenderer(Config::SCR_WIDTH, Config::SCR_HEIGHT);
+    text.load("resources/fonts/ocraext.TTF", 24);
 
-  level.load();
-  player.load("resources/models/sphere.obj", "resources/textures/food.jpg");
+    level.load();
+    player.load("./resources/models/sphere.obj", "./resources/textures/food.jpg");
 
-  for (int i = 0; i < 4; i++) {
-    switch (i) {
-      case 0:
-        ghosts.push_back(Ghost(i, "resources/models/ghost.obj", "resources/textures/ghost_texture_pink.png",
-                               glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(1.0, 0.08, 0.5), 2.0f));
-        break;
-      case 1:
-        ghosts.push_back(Ghost(i, "resources/models/ghost.obj", "resources/textures/ghost_texture_red.png",
-                               glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(1.0, 0.0, 0.0), 3.0f));
-        break;
-      case 2:
-        ghosts.push_back(Ghost(i, "resources/models/ghost.obj", "resources/textures/ghost_texture_blue.png",
-                               glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(0.0, 0.0, 1.0), 4.0f));
-        break;
-      case 3:
-        ghosts.push_back(Ghost(i, "resources/models/ghost.obj", "resources/textures/ghost_texture_orange.png",
-                               glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(1.0, 1.0, 0.0), 5.0f));
-        break;
+    for (int i = 0; i < 4; i++) {
+        switch (i) {
+            case 0:
+                ghosts.push_back(Ghost(i, "./resources/models/ghost.obj", "./resources/textures/ghost_texture_pink.png",
+                                       glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(1.0, 0.08, 0.5), 2.0f));
+                break;
+            case 1:
+                ghosts.push_back(Ghost(i, "./resources/models/ghost.obj", "./resources/textures/ghost_texture_red.png",
+                                       glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(1.0, 0.0, 0.0), 3.0f));
+                break;
+            case 2:
+                ghosts.push_back(Ghost(i, "./resources/models/ghost.obj", "./resources/textures/ghost_texture_blue.png",
+                                       glm::vec3(-1.0, 0.0f, 1 - i), glm::vec3(0.0, 0.0, 1.0), 4.0f));
+                break;
+            case 3:
+                ghosts.push_back(Ghost(i, "./resources/models/ghost.obj",
+                                       "./resources/textures/ghost_texture_orange.png", glm::vec3(-1.0, 0.0f, 1 - i),
+                                       glm::vec3(1.0, 1.0, 0.0), 5.0f));
+                break;
+        }
     }
-  }
-  score = 0;
+    score = 0;
 }
 
 void Game::update(float dt) {
-  if (score == 320) state = GAME_WIN;  // Hard coded cuz I'm lazy :)
-  checkCollision(dt);
-  if (level.update(camera.Position.x, camera.Position.z)) score++;
-  
-  for (auto &ghost : ghosts) {
-    ghost.deltaTime += dt;
-    if (ghost.deltaTime >= ghost.searchTime) {
-      ghost.deltaTime = 0;
-      int start[2] = {(int)ghost.getPosition().x, (int)ghost.getPosition().z};
-      int end[2] = {(int)camera.Position.x, (int)camera.Position.z};
-      ghost.path = route.search(level.grid, start, end);
+    if (score == 320) state = GAME_WIN;  // Hard coded cuz I'm lazy :)
+    checkCollision(dt);
+    if (level.update(camera.Position.x, camera.Position.z)) score++;
+
+    for (auto &ghost : ghosts) {
+        ghost.deltaTime += dt;
+        if (ghost.deltaTime >= ghost.searchTime) {
+            ghost.deltaTime = 0;
+            int start[2] = {(int)ghost.getPosition().x, (int)ghost.getPosition().z};
+            int end[2] = {(int)camera.Position.x, (int)camera.Position.z};
+            //ghost.path = route.search(level.grid, start, end);
+        }
+        ghost.update(dt);
+        if (glm::distance(ghost.getPosition(), camera.Position) < 0.32f) state = GAME_LOSS;
     }
-    ghost.update(dt);
-    if (glm::distance(ghost.getPosition(), camera.Position) < 0.32f) state = GAME_LOSS;
-  }
 }
 
 void Game::processInput(float dt) {
-  if (keys[GLFW_KEY_W]) camera.ProcessKeyboard(FORWARD, dt);
-  if (keys[GLFW_KEY_S]) camera.ProcessKeyboard(BACKWARD, dt);
-  if (keys[GLFW_KEY_A]) camera.ProcessKeyboard(LEFT, dt);
-  if (keys[GLFW_KEY_D]) camera.ProcessKeyboard(RIGHT, dt);
-  if (keys[GLFW_KEY_F]) flashlight = !flashlight;
-  if (keys[GLFW_KEY_M]) view = (view + 1) % 3;
-  if (keys[GLFW_KEY_LEFT_SHIFT]) {
-    camera.ProcessKeyboard(RUN, dt);
-  } else {
-    camera.ProcessKeyboard(STOP_RUN, dt);
-  }
+    if (keys[GLFW_KEY_W]) camera.ProcessKeyboard(FORWARD, dt);
+    if (keys[GLFW_KEY_S]) camera.ProcessKeyboard(BACKWARD, dt);
+    if (keys[GLFW_KEY_A]) camera.ProcessKeyboard(LEFT, dt);
+    if (keys[GLFW_KEY_D]) camera.ProcessKeyboard(RIGHT, dt);
+    if (keys[GLFW_KEY_F]) flashlight = !flashlight;
+    if (keys[GLFW_KEY_M]) view = (view + 1) % 3;
+    if (keys[GLFW_KEY_LEFT_SHIFT]) {
+        camera.ProcessKeyboard(RUN, dt);
+    } else {
+        camera.ProcessKeyboard(STOP_RUN, dt);
+    }
 }
 
 void Game::render() {
-  shader.reset();
-  setLighting();
-  setUpTransformations();
+    shader.reset();
+    setLighting();
+    setUpTransformations();
 
-  for (auto &ghost : ghosts) {
-    ghost.draw(shader);
-  }
-  level.draw(shader);
-  drawPlayer();
+    for (auto &ghost : ghosts) {
+        ghost.draw(shader);
+    }
+    level.draw(shader);
+    drawPlayer();
 
-  displayScore();
+    displayScore();
 }
 
 void Game::renderEndScreen() {
-  std::stringstream ss;
-  ss << score;
+    std::stringstream ss;
+    ss << score;
 
-  if (state == GAME_WIN) {
-    text.renderText("You win!", 320.0, Config::SCR_HEIGHT / 2 - 20.0, 1.0);
-  } else {
-    text.renderText("You lose!", 320.0, Config::SCR_HEIGHT / 2 - 20.0, 1.0);
-  }
+    if (state == GAME_WIN) {
+        text.renderText("You win!", 320.0, Config::SCR_HEIGHT / 2 - 20.0, 1.0);
+    } else {
+        text.renderText("You lose!", 320.0, Config::SCR_HEIGHT / 2 - 20.0, 1.0);
+    }
 
-  text.renderText("Score:" + ss.str(), 325.0, Config::SCR_HEIGHT / 2, 1.0);
+    text.renderText("Score:" + ss.str(), 325.0, Config::SCR_HEIGHT / 2, 1.0);
 }
 
 void Game::setUpTransformations() {
-  auto projection = glm::perspective(glm::radians(50.f), 16.f / 9.f, 0.01f, 650.f);
-  auto lookAt = glm::mat4(1.0f);
+    auto projection = glm::perspective(glm::radians(50.f), 16.f / 9.f, 0.01f, 650.f);
+    auto lookAt = glm::mat4(1.0f);
 
-  switch (view) {
-    case FIRST_PERSON:
-      lookAt = camera.GetFirstPersonView();
-      break;
-    case THIRD_PERSON:
-      lookAt = camera.GetThirdPersonView();
-      break;
-    case BIRD_VIEW:
-      lookAt = camera.Get2DView();
-      break;
-  }
+    switch (view) {
+        case FIRST_PERSON:
+            lookAt = camera.GetFirstPersonView();
+            break;
+        case THIRD_PERSON:
+            lookAt = camera.GetThirdPersonView();
+            break;
+        case BIRD_VIEW:
+            lookAt = camera.Get2DView();
+            break;
+    }
 
-  shader.setMat4("view", lookAt);
-  shader.setMat4("projection", projection);
+    shader.setMat4("view", lookAt);
+    shader.setMat4("projection", projection);
 }
 
 void Game::setLighting() {
-  shader.setDirLight(glm::vec3(-0.2f, -1.0f, -0.3f), glm::vec3(0.0f), glm::vec3(0.35f), glm::vec3(0.5f));
-  shader.setSpotLight(camera.Position, camera.Front);
-  shader.toggleFlashlight(flashlight);
+    shader.setDirLight(glm::vec3(-0.2f, -1.0f, -0.3f), glm::vec3(0.0f), glm::vec3(0.35f), glm::vec3(0.5f));
+    shader.setSpotLight(camera.Position, camera.Front);
+    shader.toggleFlashlight(flashlight);
 }
 
 void Game::displayScore() {
-  std::stringstream ss;
-  ss << this->score;
-  text.renderText("Score: " + ss.str(), 5.0f, 5.0f, 1.0);
+    std::stringstream ss;
+    ss << this->score;
+    text.renderText("Score: " + ss.str(), 5.0f, 5.0f, 1.0);
 }
 
 void Game::checkCollision(float dt) {
-  for (auto &tile : level.grid) {
-    if (tile.second) {
-      if (glm::distance(camera.Position, glm::vec3(tile.first.first, 0.0f, tile.first.second)) <= 0.7) {
-        if (keys[GLFW_KEY_W]) camera.ProcessKeyboard(BACKWARD, dt);
-        if (keys[GLFW_KEY_S]) camera.ProcessKeyboard(FORWARD, dt);
-        if (keys[GLFW_KEY_A]) camera.ProcessKeyboard(RIGHT, dt);
-        if (keys[GLFW_KEY_D]) camera.ProcessKeyboard(LEFT, dt);
-      }
+    for (auto &tile : level.grid) {
+        if (tile.second) {
+            if (glm::distance(camera.Position, glm::vec3(tile.first.first, 0.0f, tile.first.second)) <= 0.7) {
+                if (keys[GLFW_KEY_W]) camera.ProcessKeyboard(BACKWARD, dt);
+                if (keys[GLFW_KEY_S]) camera.ProcessKeyboard(FORWARD, dt);
+                if (keys[GLFW_KEY_A]) camera.ProcessKeyboard(RIGHT, dt);
+                if (keys[GLFW_KEY_D]) camera.ProcessKeyboard(LEFT, dt);
+            }
+        }
     }
-  }
 }
 
 void Game::drawPlayer() {
-  if (view != FIRST_PERSON) {
-    player.draw(camera.Position, 0.06, 0, shader);
-  }
+    if (view != FIRST_PERSON) {
+        player.draw(camera.Position, 0.06, 0, shader);
+    }
 }
